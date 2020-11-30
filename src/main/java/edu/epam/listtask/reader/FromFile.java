c2043f67bbeba82c3fcf143b317229afb829439d
6opo9a4 <maksimazyr@gmail.com>
2020-11-30 17:14:33 +0300

    v1.5.0

--- src/main/java/edu/epam/listtask/reader/FromFile.java
+++ src/main/java/edu/epam/listtask/reader/FromFile.java
@@ -1,4 +1,4 @@
-package edu.epam.listtask.creator;
+package edu.epam.listtask.reader;
 
 import edu.epam.listtask.variables.ConstantValue;
 import edu.epam.listtask.variables.Student;
@@ -6,31 +6,19 @@ import edu.epam.listtask.variables.University;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
-public class UniversityFromFile {
+public class FromFile {
     private static final Logger log = LogManager.getLogger(String.valueOf(Student.class));
     ConstantValue constant = new ConstantValue();
 
-    public void writeToFile (String filename,University university) {
-        try {
-            FileOutputStream f = new FileOutputStream(new File(constant.getFILE_PATH()+filename+".txt"));
-            ObjectOutputStream o = new ObjectOutputStream(f);
-            o.writeObject(university);
-            o.close();
-            f.close();
-        } catch (IOException e) {
-            log.error("Error: " + e.getMessage());
-        }
-    }
-
-    public University readFromFile(String filename) {
+    public University readUniversityFromFile(String filename) {
         University university = new University();
         try {
             FileInputStream fi = new FileInputStream(new File(constant.getFILE_PATH()+filename + ".txt"));
@@ -47,5 +35,22 @@ public class UniversityFromFile {
             e.printStackTrace();
         }
         return university;
+    }
+
+    public Student readStudentFromFile(String filename) {
+        Student student = new Student();
+        try {
+        FileInputStream fi = new FileInputStream(new File(constant.getFILE_PATH()+filename + ".txt"));
+        ObjectInputStream oi = new ObjectInputStream(fi);
+        student = (Student) oi.readObject();
+        oi.close();
+        fi.close();
+        log.info("User read from file");
+        } catch (FileNotFoundException e) {
+            log.error("Error: " + e.getMessage());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return student;
     }
 }
